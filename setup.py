9e89d40df859fa6da7ac38f25c1564ee5f009d0c
Dean <deanfreekinggalvin@gmail.com>
2016-01-21 21:30:57 -0500

    added readme back to long desc

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
 from setuptools import setup
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name = "insteon-hub",
     version = "0.3.0",
@@ -11,7 +14,7 @@ setup(
     keywords = "insteon hub cloud",
     url = "https://github.com/FreekingDean/insteon-hub",
     packages=['insteon'],
-    long_description=read('README.md').read(),
+    long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Home Automation",
